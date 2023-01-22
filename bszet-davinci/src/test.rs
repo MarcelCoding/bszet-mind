25add1d4ee043b588bf5377892c10ef22d9d02e1
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2023-01-22 15:31:22 +0100

    Formatted code & fixed scheduling

--- bszet-davinci/src/test.rs
+++ bszet-davinci/src/test.rs
@@ -2,22 +2,22 @@ use crate::davinci::Davinci;
 
 #[tokio::test]
 async fn test_load() -> anyhow::Result<()> {
-    let mut davinci = Davinci::new(
-        "https://geschuetzt.bszet.de/s-lk-vw/Vertretungsplaene/V_PlanBGy/V_DC_001.html"
-            .parse()
-            .unwrap(),
-        "bsz-et-2122".to_string(),
-        "impfung#21".to_string(),
-    );
+  let mut davinci = Davinci::new(
+    "https://geschuetzt.bszet.de/s-lk-vw/Vertretungsplaene/V_PlanBGy/V_DC_001.html"
+      .parse()
+      .unwrap(),
+    "bsz-et-2122".to_string(),
+    "impfung#21".to_string(),
+  );
 
-    println!("{:?}", davinci.update().await?);
+  println!("{:?}", davinci.update().await?);
 
-    // assert_eq!(true, davinci.update().await?);
-    // assert_eq!(false, davinci.update().await?);
+  // assert_eq!(true, davinci.update().await?);
+  // assert_eq!(false, davinci.update().await?);
 
-    for row in &davinci.data().unwrap().rows {
-        println!("- {:?}", row);
-    }
+  for row in &davinci.data().unwrap().rows {
+    println!("- {:?}", row);
+  }
 
-    Ok(())
+  Ok(())
 }
